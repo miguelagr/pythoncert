51d2a9b089b316cea295ab3a73810fca0e643be0
miguelagr <miguelagr23@gmail.com>
2017-12-14 00:40:49 -0600

    Nueva actualizacion

---
+++ ejercicio4/ejercicio4.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#UNAM-CERT
+from random import choice
+from poo1 import *
+
+calificacion_alumno = {}
+calificaciones = (0,1,2,3,4,5,6,7,8,9,10)
+becarios = ['Alonso',
+            'Eduardo',
+            'Gerardo',
+            'Rafael',
+            'Antonio',
+            'Fernanda',
+            'Angel',
+            'Itzel',
+            'Karina',
+            'Esteban',
+            'Alan',
+            'Samuel',
+            'Jose',
+            'Guadalupe',
+            'Angel',
+            'Ulises']
+
+def asigna_calificaciones():
+    obbecarios=[]
+    for b in becarios:
+#        print b
+        bec=Becario(b,choice(calificaciones))
+#        print bec
+        obbecarios.append(bec)
+    return obbecarios
+#def imprime_calificaciones():
+#    for alumno in calificacion_alumno:
+#        print '%s tiene %s\n' % (alumno,calificacion_alumno[alumno])
+
+oob=asigna_calificaciones()
+print oob[5]
+#imprime_calificaciones()
